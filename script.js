8f63e4f747907c3ba785fd6d7d32231f2771276f
muaazjangda58-cmyk <muaazjangda58@gmail.com>
2026-02-21 09:27:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,162 @@
+// CHAPTER 1: ALERTS
+
+// Question # 1
+// alert("Hello, M.Muaaz!")
+
+// Question # 2
+// alert("Error! Please enter a valid password")
+
+// Question # 3
+// alert("Hello  and Welcome! \nHave a wonderful day ahead.")
+
+// Question # 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Question # 5
+// alert("Hello... I can run JS through my web brower's console") "Ye code console me likha tha, kaam kar raha hai 👍🏻"
+
+// Question # 6
+
+// Question # 7
+// d. Body (after your page’s HTML)
+
+// CHAPTER 2: VARIABLES FOR STRINGS
+
+// Question # 1
+// var username
+
+// Question # 2
+// var myName = "M.Muaaz Shabbir"
+
+// Question # 3
+// var message = "Hello World"
+// alert(message)
+
+// Question # 4
+// var studentName = "M.Muaaz Shabbir"
+// var studentAge = "18 years old"
+// var course = "Web and Mobile Application Development"
+// alert(studentName)
+// alert(studentAge)
+// alert(course)
+
+// Question # 5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+
+// Question # 6
+// var email = "muaazjangda58@gmail.com"
+// alert("My email address is " + email)
+
+// Question # 7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book)
+
+// Question # 8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Question # 9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design)
+// document.write(design)
+
+// CHAPTER 3: VARIABLES FOR NUMBERS
+
+// Question # 1
+// var age = 18
+// alert("I am " + age + " years old")
+
+// Question # 2
+// var visitCount = 14
+// alert("You have visited this site " + visitCount + " times")
+
+// Question # 3
+// var birthYear = 2007
+// document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is number")
+
+// Question # 4
+// var visitorsName = "M.Muaaz Shabbir"
+// var prodrctTitle = "T-shirt(s)"
+// var quantity = 5
+// document.write(visitorsName + " ordered " + quantity + " " + prodrctTitle + " on XYZ Clothing store.")
+
+// CHAPTER 4: VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Question # 1
+// var name, age, course
+
+// Quetion # 2
+// Legal variable names
+// var username
+// var userName
+// var user_name
+// var UserName
+// var USER_NAME
+
+// Illegal variable names
+// var 1username
+// var user-name
+// var user name
+// var user@name
+// var user.name
+
+// Question # 3
+// document.write("Rules for naming JS variables" + "<br>" + "<br>")
+// document.write("Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable" + "<br>")
+// document.write("Variables must begin with a letter, $ or _. For example: $name, _name or name" + "<br>")
+// document.write("Variable names are sensitive " + "<br>")
+// document.write("Variable names should not be JavaScript keywords" + "<br>")
+
+// CHAPTER 5: MATH EXPRESSIONS
+
+// Question # 1
+// var num1 = 3
+// var num2 = 5
+// var sum = num1 + num2
+// document.write("Sum of " + num1 + " and " + num2 + " is " + sum)
+
+// Question # 2
+// var num1 = 10
+// var num2 = 5
+// var sub = num1 - num2
+// var mul = num1 * num2
+// var div = num1 / num2
+// var mod = num1 % num2
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + sub + "<br>")
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + mul + "<br>")
+// document.write("Division of " + num1 + " and " + num2 + " is " + div + "<br>")
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + mod)
+
+// Question # 3
+// var num
+// document.write("Value after variable declaration is: " + num + "<br>")
+// num = 5
+// document.write("Initial value: " + num + "<br>")
+// num++
+// document.write("Value after increment is: " + num + "<br>")
+// num = num + 7
+// document.write("Value after addition is: " + num + "<br>")
+// num--
+// document.write("Value after decrement is: " + num + "<br>")
+// num = num % 3
+// document.write("The remainder is: " + num)
+
+// Question # 4
+// var ticketPrice = 600
+// var totalCost = ticketPrice * 5
+// document.write("Total cost to buy 5 tickets to a movie is " + totalCost + "PKR")
+
+// Question # 5
+// var num = 4
+// document.write("Table of " + num + "<br><br>")
+// document.write(num + " x 1 = " + num * 1 + "<br>")
+// document.write(num + " x 2 = " + num * 2 + "<br>")
+// document.write(num + " x 3 = " + num * 3 + "<br>")
+// document.write(num + " x 4 = " + num * 4 + "<br>")
+// document.write(num + " x 5 = " + num * 5 + "<br>")
+// document.write(num + " x 6 = " + num * 6 + "<br>")
+// document.write(num + " x 7 = " + num * 7 + "<br>")
+// document.write(num + " x 8 = " + num * 8 + "<br>")
+// document.write(num + " x 9 = " + num * 9 + "<br>")
+// document.write(num + " x 10 = " + num * 10)
